e999727c72a4780813776d8ccd6373c93d6a2382
officiallymono <officiallymono@gmail.com>
2025-02-02 03:12:59 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-// next.config.js
-
 module.exports = {
   reactStrictMode: true,
   env: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
   },
-}
+  swcMinify: true,
+  images: {
+    domains: ['i.scdn.co'],
+  },
+  webpack(config) {
+    return config;
+  }
+};
