f65a34495b4ac729baa3f48ba8c7fc4ab3b9137d
officiallymono <officiallymono@gmail.com>
2025-02-02 02:56:23 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
+// next.config.js
 
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+  },
+}
